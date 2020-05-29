81f98c7836b1a4bd321943ed522049f3948d343c
Muhammad Salif <muhammad.salif@yahoo.com>
2020-05-29 19:38:37 +0500

    NavBar Done.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import logo from "../images/logo.svg";
+import { Link } from "react-router-dom";
 
 function NavBar() {
-  return <div>Hello From NavBar</div>;
+  return (
+    <nav className="navbar navbar-expand-sm navbar-light bg-light">
+      <Link to="/" className="navbar-brand">
+        <img src={logo} alt="Food2Fork" />
+      </Link>
+      <div className="collapse navbar-collapse show ml-sm-5 ">
+        <ul className="navbar-nav ">
+          <li className="navbar-item ">
+            <Link className="navbar-link " to="/">
+              Home
+            </Link>
+          </li>
+          <li className="navbar-item ml-sm-5">
+            <Link className="navbar-link" to="/recipes">
+              Recipes
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
 export default NavBar;
