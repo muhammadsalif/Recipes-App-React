d10da8f3d8911123a78373916c692e17f8c07c33
Muhammad Salif <muhammad.salif@yahoo.com>
2020-05-29 22:25:38 +0500

    Home page done.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,19 @@
 import React, { Component } from "react";
+import Header from "../components/Header";
+import { Link } from "react-router-dom";
 
 class Home extends Component {
   render() {
     return (
       <div>
-        <h1>Hello From Home.jsx</h1>
+        <Header title="Amazing recipes">
+          <Link
+            to="recipes"
+            className="text-uppercase btn btn-secondary btn-lg mt-3"
+          >
+            Search Recipes
+          </Link>
+        </Header>
       </div>
     );
   }
