9e2d9ff517479a3995b53ce84e207ad41db7d8d8
Muhammad Salif <muhammad.salif@yahoo.com>
2020-06-02 15:04:46 +0500

    Mobile issue resolved.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ function Header({ children, title, styleClass }) {
       <div className="container-fluid">
         <div className={`row align-items-center ${styleClass}`}>
           <div className="col text-center">
-            <h1 className="text-light text-uppercase display-3 letter-spacing text-slanted">
+            <h1 className="text-light text-uppercase display-4 letter-spacing text-slanted">
               {title}
             </h1>
             {children}
