bc62ee0b4ad01682a5e060ec60b1f35eb46bd2fd
Muhammad Salif <muhammad.salif@yahoo.com>
2020-06-05 17:11:42 +0500

    Search component done dynamically.

--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -12,15 +12,26 @@ class Recipes extends Component {
     recipes: {},
     search: "",
     url: `https://recipesapi.herokuapp.com/api/search`,
+    base_url: `https://recipesapi.herokuapp.com/api/search`,
+    query: "&q=",
+    error: "",
   };
 
   async getRecipes() {
     try {
       const data = await fetch(this.state.url);
       const jsonData = await data.json();
-      this.setState({
-        recipes: jsonData.recipes,
-      });
+      if (jsonData.recipes.length === 0) {
+        this.setState({
+          error:
+            "Sorry! but your search did not return any recipes, Please try again or press search icon for the most popular recipes",
+        });
+      } else {
+        this.setState({
+          recipes: jsonData.recipes,
+          error: "",
+        });
+      }
     } catch (error) {
       console.log(error);
     }
@@ -37,6 +48,14 @@ class Recipes extends Component {
   };
   handleSubmit = (e) => {
     e.preventDefault();
+    const { base_url, query, search } = this.state;
+    this.setState(
+      {
+        url: `${base_url}${query}${search}`,
+        search: "",
+      },
+      () => this.getRecipes()
+    );
   };
 
   render() {
@@ -47,7 +66,19 @@ class Recipes extends Component {
           handleChange={this.handleChange}
           handleSubmit={this.handleSubmit}
         ></Search>
-        <RecipeList recipe={this.state.recipes}></RecipeList>
+        {this.state.error ? (
+          <section>
+            <div className="row">
+              <div className="col">
+                <h2 className="text-orange text-center text-uppercase mt-5">
+                  {this.state.error}
+                </h2>
+              </div>
+            </div>
+          </section>
+        ) : (
+          <RecipeList recipe={this.state.recipes}></RecipeList>
+        )}
       </div>
     );
   }
