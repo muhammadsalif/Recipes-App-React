c1e33e7e87c2df51ed5d53a3dc67d460b02921d5
Muhammad Salif <muhammad.salif@yahoo.com>
2020-06-05 16:34:42 +0500

    Recipes Ajax request code done.

--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -1,15 +1,35 @@
 import React, { Component } from "react";
 import Search from "../components/Search";
 import RecipeList from "../components/RecipeList";
-import { recipeData } from "./../data/tempList";
+// import { recipeData } from "./../data/tempList";
 class Recipes extends Component {
   constructor(props) {
     super(props);
+    this.getRecipes = this.getRecipes.bind(this);
   }
   state = {
-    recipes: recipeData,
+    // recipes: recipeData,
+    recipes: {},
     search: "",
+    url: `https://recipesapi.herokuapp.com/api/search`,
   };
+
+  async getRecipes() {
+    try {
+      const data = await fetch(this.state.url);
+      const jsonData = await data.json();
+      this.setState({
+        recipes: jsonData.recipes,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  componentDidMount() {
+    this.getRecipes();
+  }
+
   handleChange = (e) => {
     this.setState({
       search: e.target.value,
